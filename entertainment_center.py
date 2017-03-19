4825d90ab2948355c587726951b2ce456bf932cc
Caner Ertano <canerertano@gmail.com>
2017-03-19 15:51:19 +0300

    refactor: move some code into a function

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,20 +2,26 @@ import json
 import media
 import fresh_tomatoes
 
-# open the file which movies's information stored in.
-file_stream = open("movie_db.json", "r")
+def prepare_movie_list():
 
-# read the file and parse into json object.
-json_data = json.loads(file_stream.read())
+	# open the file which movies's information stored in.
+	file_stream = open("movie_db.json", "r")
 
-# get the movie list as json object.
-movies_json = json_data["movies"]
+	# read the file and parse into json object.
+	json_data = json.loads(file_stream.read())
 
-# convert plain json to Movie class instances and add into movie list.
-movies = []
-for movie_json in movies_json:
-    movies.append(media.Movie(movie_json["title"], movie_json["year"], movie_json["storyline"], movie_json["poster_image_url"],
-                              movie_json["trailer_youtube_url"]))
+	# get the movie list as json object.
+	movies_json = json_data["movies"]
+
+	# convert plain json to Movie class instances and add into movie list.
+	movies = []
+	for movie_json in movies_json:
+	    movies.append(media.Movie(movie_json["title"], movie_json["year"], movie_json["storyline"], movie_json["poster_image_url"],
+	                              movie_json["trailer_youtube_url"]))
+	return movies
+
+# call the function that creates movie list
+movies = prepare_movie_list()
 
 # call the function that generates web page and pass the list of movies
 fresh_tomatoes.open_movies_page(movies)
