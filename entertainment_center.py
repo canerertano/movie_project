74a0a92031e36c8ff84dc17a741d66104274a780
Caner Ertano <canerertano@gmail.com>
2017-03-19 16:09:30 +0300

    style: changes for Pep8

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,23 +2,26 @@ import json
 import media
 import fresh_tomatoes
 
+
 def prepare_movie_list():
 
-	# open the file which movies's information stored in.
-	file_stream = open("movie_db.json", "r")
+    # open the file which movies's information stored in.
+    file_stream = open("movie_db.json", "r")
 
-	# read the file and parse into json object.
-	json_data = json.loads(file_stream.read())
+    # read the file and parse into json object.
+    json_data = json.loads(file_stream.read())
 
-	# get the movie list as json object.
-	movies_json = json_data["movies"]
+    # get the movie list as json object.
+    movies_json = json_data["movies"]
 
-	# convert plain json to Movie class instances and add into movie list.
-	movies = []
-	for movie_json in movies_json:
-	    movies.append(media.Movie(movie_json["title"], movie_json["year"], movie_json["storyline"], movie_json["poster_image_url"],
-	                              movie_json["trailer_youtube_url"]))
-	return movies
+    # convert plain json to Movie class instances and add into movie list.
+    movies = []
+    for movie_json in movies_json:
+        movies.append(media.Movie(movie_json["title"], movie_json["year"],
+                                  movie_json["storyline"],
+                                  movie_json["poster_image_url"],
+                                  movie_json["trailer_youtube_url"]))
+    return movies
 
 # call the function that creates movie list
 movies = prepare_movie_list()
